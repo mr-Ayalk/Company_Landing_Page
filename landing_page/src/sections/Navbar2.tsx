9e59a03686032102aa954a0b92285801d112f101
mr-Ayalk <ayalkteketel7@gmail.com>
2025-12-27 14:42:54 +0300

    the logo on the navbar2 is updated

--- landing_page/src/sections/Navbar2.tsx
+++ landing_page/src/sections/Navbar2.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useState, useEffect } from "react";
-import logoImage from "../assets/images/eyohalogo.png";
+import logoImage from "../assets/images/eyohalogonew.png";
 
 import Link from "next/link";
 import Image from "next/image";
